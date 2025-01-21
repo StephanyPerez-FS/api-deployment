b89b1d07f92740aa09d0c3e2017a92d58db4e0ed
Stephany Perez <saperezbarrios@student.fullsail.edu>
2025-01-20 20:38:44 -0500

    front end in process

--- api/server.js
+++ api/server.js
@@ -19,7 +19,13 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Database Connection Established"));
 
 app.use(express.json());
-app.use("/cars", carRouter);
+app.use("/api/v1/cars", carRouter);
+
+app.use(express.static(path.join(__dirname, "../reactjs/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../reactjs/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
